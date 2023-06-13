f8e8cd3b40366c5e69f6c2663c154b8ca06f6573
Goldendenday <bieltikov99@mail.ru>
2023-06-14 01:39:02 +0300

    Make TASK2 HW

---
+++ HomeWork/TASK2/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите цифру от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 || number > 7)
+{
+    System.Console.WriteLine("Введи коректную цифру!");
+}
+else if (number == 6 || number == 7)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}
