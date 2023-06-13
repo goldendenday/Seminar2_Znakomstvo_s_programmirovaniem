ffc25617650619f83209fc5ddf40ed0dc24c315b
Goldendenday <bieltikov99@mail.ru>
2023-06-14 01:02:56 +0300

    Make Task1 HW

--- HomeWork/TASK1/Program.cs
+++ HomeWork/TASK1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 1000 || number < 100)
+{
+    System.Console.WriteLine("ОШИБКА!Введи трехзначное");
+}
+else
+{
+    System.Console.WriteLine(number%100/10);
+}
