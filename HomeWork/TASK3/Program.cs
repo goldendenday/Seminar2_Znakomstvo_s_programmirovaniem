68f70599f2b328cb8ea593c8c6d65d4f2004bba9
Goldendenday <bieltikov99@mail.ru>
2023-06-14 10:52:58 +0300

    Make TASK3 HW

---
+++ HomeWork/TASK3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+Console.WriteLine("Введи число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+if (number < 1000 && number >=100)
+{
+    System.Console.WriteLine(number%10);
+}
+if (number >= 1000 && number < 10000)
+{
+    System.Console.WriteLine(number%100/10);
+}
+if (number >= 10000 && number < 100000)
+{
+    System.Console.WriteLine(number%1000/100);
+}
